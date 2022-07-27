88a528f23df0c57fd8263b9fddc79c380f7e8772
Ester Oliveira <86311735+esteroliver@users.noreply.github.com>
2022-07-27 20:27:38 -0300

    configuração do componente

--- app/src/app/exibir-mensagem/exibir-mensagem.component.ts
+++ app/src/app/exibir-mensagem/exibir-mensagem.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exibir-mensagem.component.css']
 })
 export class ExibirMensagemComponent implements OnInit {
-
-  constructor() { }
+  mensagem: string
+  constructor() { 
+    this.mensagem = ''
+  }
 
   ngOnInit(): void {
   }
-
+  alterarMensagem(n:string){
+    this.mensagem = `Seja bem-vindo(a), ${n}!`
+  }
 }
