ea2c998de002a2b5297b470279022688dad49f4f
Ester Oliveira <86311735+esteroliver@users.noreply.github.com>
2022-07-28 14:00:39 -0300

    componente para calcular média

--- media/src/app/calcular-media/calcular-media.component.ts
+++ media/src/app/calcular-media/calcular-media.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calcular-media.component.css']
 })
 export class CalcularMediaComponent implements OnInit {
-
-  constructor() { }
+  media: string
+  constructor() { 
+    this.media = ''
+  }
 
   ngOnInit(): void {
+  }
+  mediaAluno(a: any, b: any, c: any, d: any){
+    var media_aluno: number = ((a * 2) + (b * 2) + (c * 3) + (d * 3))/10
+    this.media = `A média do aluno é: ${media_aluno}`
+
   }
 
 }
